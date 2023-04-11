8278da16293027c6d1e780a4a9fabdf720e8ba72
Analise <analise.giobbi@gc.com>
2023-04-11 15:35:36 -0400

    work on the schemas and server refactor

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const { gql } = require('apollo-server-express')
+
+const typeDefs = `
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        savedBooks: [Book]!
+    }
+
+    type Book {
+        _id: ID
+        authors: String
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        users: [User]
+        user(username: String!): User
+        books: [Book]
+        book (bookId: ID!): Book
+        me: User
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        saveBook(bookId: ID!): Book
+        removeBook(bookId: ID!): Book
+    }
+`
+module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express')
 const path = require('path');
+//uncomment when you have built out these two files
+const { typeDefs, resolvers } = require('./schema')
 const db = require('./config/connection');
-const routes = require('./routes');
+const { __Directive } = require('graphql');
+//no longer need this with the resolvers
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//uncomment this when you have the files ready
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +26,25 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('/', (req, res) =>{
+  res.sendFile(path.json(__dirname, '../client/build/index.js'))
+})
+
+//uncomment when you have the middleware built out 
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app })
+}
+
+
+
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  //allows you to use sandbox if need to test
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
+
+//uncomment when done with other work
+startApolloServer(typeDefs, resolvers)
